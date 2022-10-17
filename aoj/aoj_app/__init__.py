c51314e0105785d2503c732a89104f2e963482ec
rahul-snakescript <113518994+rahul-snakescript@users.noreply.github.com>
2022-10-17 11:10:42 +0530

    Delete aoj directory

--- aoj/aoj_app/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'aoj_app.apps.AojAppConfig'
--- aoj/aoj_app/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class AojAppConfig(AppConfig):
-    name = 'aoj_app'
-    verbose_name = 'Arms Of Jesus APP'
